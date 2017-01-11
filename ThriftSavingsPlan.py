495339bfa6a1c8feba29f553c7c351e33d66775f
James Phillips <jrphill5@users.noreply.github.com>
2017-01-10 22:09:23 -0500

    Fixed tabbing error due to stupidity

--- ThriftSavingsPlan.py
+++ ThriftSavingsPlan.py
@@ -29,16 +29,47 @@ else:
         if len(kn) > 0:
             TSP[kn] = v
 
-        TSP['date'] = [datetime.strptime(date, '%Y-%m-%d') for date in TSP['date']]
+    TSP['date'] = [datetime.strptime(date, '%Y-%m-%d') for date in TSP['date']]
 
-        dates = date2num(TSP['date'])
+    dates = date2num(TSP['date'])
 
-        plt.title('Thrift Savings Plan Funds from ' + TSP['date'][0].strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
-        plt.plot_date(dates, TSP['G Fund'], '-', label="G Fund")
-        plt.plot_date(dates, TSP['F Fund'], '-', label="F Fund")
-        plt.plot_date(dates, TSP['C Fund'], '-', label="C Fund")
-        plt.plot_date(dates, TSP['S Fund'], '-', label="S Fund")
-        plt.plot_date(dates, TSP['I Fund'], '-', label="I Fund")
+    plt.title('Thrift Savings Plan Funds from ' + TSP['date'][0].strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
+    plt.plot_date(dates, TSP['G Fund'], '-', label="G Fund")
+    plt.plot_date(dates, TSP['F Fund'], '-', label="F Fund")
+    plt.plot_date(dates, TSP['C Fund'], '-', label="C Fund")
+    plt.plot_date(dates, TSP['S Fund'], '-', label="S Fund")
+    plt.plot_date(dates, TSP['I Fund'], '-', label="I Fund")
+
+    #manager = plt.get_current_fig_manager()
+    #manager.resize(*manager.window.maxsize())
+    #manager.window.wm_geometry("+0+0")
+
+    plt.xlabel('Date')
+    plt.ylabel('Share Value ($)')
+
+    plt.legend(loc=2)
+    plt.show(block=True)
+
+    plt.cla()
+
+    nl = 10
+    nh = 30
+
+    for fund in ['G', 'F', 'C', 'S', 'I']:
+        datesall = date2num(TSP['date'])
+        datesnl = date2num(TSP['date'][nl - 1:])
+        datesnh = date2num(TSP['date'][nh - 1:])
+
+        #t = np.array(date2num(TSP['date'][nh - 1:]))
+        #f = np.array(SMA(TSP[fund + ' Fund'], nl))
+        #g = np.array(SMA(TSP[fund + ' Fund'], nh))
+
+        #print(len(t), len(f), len(g))
+
+        plt.title('Thrift Savings Plan ' + fund + ' Fund from ' + TSP['date'][0].strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
+        plt.plot_date(datesall, TSP[fund + ' Fund'], '-', label=fund + " Fund")
+        plt.plot_date(datesnl, SMA(TSP[fund + ' Fund'], nl), '-', label=fund + " Fund (" + str(nl) + " day)")
+        plt.plot_date(datesnh, SMA(TSP[fund + ' Fund'], nh), '-', label=fund + " Fund (" + str(nh) + " day)")
 
         #manager = plt.get_current_fig_manager()
         #manager.resize(*manager.window.maxsize())
@@ -49,28 +80,3 @@ else:
 
         plt.legend(loc=2)
         plt.show(block=True)
-
-        plt.cla()
-
-        nl = 10
-        nh = 30
-
-        for fund in ['G', 'F', 'C', 'S', 'I']:
-            datesall = date2num(TSP['date'])
-            datesnl = date2num(TSP['date'][nl - 1:])
-            datesnh = date2num(TSP['date'][nh - 1:])
-
-            plt.title('Thrift Savings Plan ' + fund + ' Fund from ' + TSP['date'][0].strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
-            plt.plot_date(datesall, TSP[fund + ' Fund'], '-', label=fund + " Fund")
-            plt.plot_date(datesnl, SMA(TSP[fund + ' Fund'], nl), '-', label=fund + " Fund (" + str(nl) + " day)")
-            plt.plot_date(datesnh, SMA(TSP[fund + ' Fund'], nh), '-', label=fund + " Fund (" + str(nh) + " day)")
-
-            #manager = plt.get_current_fig_manager()
-            #manager.resize(*manager.window.maxsize())
-            #manager.window.wm_geometry("+0+0")
-
-            plt.xlabel('Date')
-            plt.ylabel('Share Value ($)')
-
-            plt.legend(loc=2)
-            plt.show(block=True)
