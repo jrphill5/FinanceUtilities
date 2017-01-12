0b62106c35c9ceb2ce9df8a6269d6dce5d8d3213
Jay Phillips <rocketman8473@gmail.com>
2017-01-12 11:52:21 -0500

    Cleaned up code and added comments

--- ThriftSavingsPlan.py
+++ ThriftSavingsPlan.py
@@ -1,153 +1,181 @@
-import requests, pandas
-from io import StringIO
+import requests, pandas, time, sys, os
 from datetime import datetime, timedelta
-import time
+from io import StringIO
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import date2num, num2date, MonthLocator, WeekdayLocator, DateFormatter
-import numpy as np
-import sys, os
 
-dd = 365; # number of days to plot
-nl = 10 # time period in days for short term moving average
-nh = 30 # time period in days for long term moving average
+dd = 365 # Number of days to plot
+nl = 10  # Time period in days for short term moving average
+nh = 30  # Time period in days for long term moving average
 
-imgpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
-if not os.path.isdir(imgpath): os.mkdir(imgpath)
-
+# Define a simple moving average that replaces invalid positions with NaN:
 def SMA(list, n):
+	# Start with empty list and fill invalid values first:
 	ma = [np.nan]*n
+	# Move through the valid positions in list and compute moving average:
 	for i in range(n, len(list)):
 		ma.append(np.mean(list[i-n:i]))
+	# Return result"
 	return ma
 
-todaydt = datetime.now()
-startdt = datetime.now() - timedelta(days=dd+7.0/5.0*nh+1.0+3.0) # account for weekends and holidays
+# Detect buy and sell crossovers of two SMA lists and return signals:
+def detectCrossovers(dates, smanl, smanh):
+	# Create empty data structure:
+	crossovers = [[[],[]],[[],[]]]
 
+	# Detect change in sign at every point in the difference of two source lists:
+	for i in np.argwhere(np.diff(np.sign((smanl-smanh)[nh:])) != 0).reshape(-1) + nh:
+		# If short term SMA is below long term SMA, signal to buy:
+		if smanl[i] < smanh[i]:
+			j = 0
+			sys.stdout.write("B: ")
+		# If short term SMA is below long term SMA, signal to sell:
+		else:
+			j = 1
+			sys.stdout.write("S: ")
+
+		# Compute slopes for both short term and long term SMA:
+		smanlm = (smanl[i+1]-smanl[i])/(dates[i+1]-dates[i])
+		smanhm = (smanh[i+1]-smanh[i])/(dates[i+1]-dates[i])
+
+		# Compute exact time and value of the crossover:
+		t = (smanh[i]-smanl[i])/(smanlm-smanhm)+dates[i]
+		p = smanlm*(t-dates[i])+smanl[i]
+
+		# Append the crossover value to the data structure:
+		crossovers[j][0].append(t)
+		crossovers[j][1].append(p)
+
+		# Print the crossover date:
+		print(num2date(t).strftime("%m/%d/%Y"))
+
+	# Return the completed data structure
+	return crossovers
+
+# Create datetime object for today
+todaydt = datetime.now()
+
+# Create datetime object for the day dd days in the past accounting for loss due to moving average:
+startdt = datetime.now() - timedelta(days=dd+7.0/5.0*nh+1.0+3.0) # Take weekends and holidays into account
+
+# Convert datetime objects into format required by TSP fields:
 today = todaydt.strftime("%m/%d/%Y")
 start = startdt.strftime("%m/%d/%Y")
 
+# POST values to remote webserver and download CSV reply:
+sys.stdout.write('Fetching TSP data from ' + start + ' to ' + today + ' ... ')
+url = "https://www.tsp.gov/InvestmentFunds/FundPerformance/index.html"
+data = {'whichButton': 'CSV', 'startdate': start, 'enddate': today}
+response = requests.post(url, data=data)
+print('Response ' + str(response.status_code))
+
+# If data cannot be retreived, exit the program with an error:
+if response.status_code != 200: sys.exit("Could not retrieve data from remote server.")
+
+# Read in dataframe from CSV response and sort by date:
+df = pandas.read_csv(StringIO(response.text)).sort_values('date')
+	
+# Clean up text in dataframe and create a dictionary:
+TSP = {}
+for k, v in df.to_dict('list').items():
+	kn = k.strip()
+	if len(kn) > 0:
+		TSP[kn] = v
+
+# Convert all text dates into datetime objects:
+TSP['date'] = [datetime.strptime(date, '%Y-%m-%d') for date in TSP['date']]
+
+# Define locators and formatters for time axis of plots:
 quarters = MonthLocator(range(1, 13), bymonthday=1, interval=3)
 months = MonthLocator(range(1, 13), bymonthday=1, interval=1)
 datefmt = DateFormatter("%b %Y")
 
-url = "https://www.tsp.gov/InvestmentFunds/FundPerformance/index.html"
-data = {'whichButton': 'CSV', 'startdate': start, 'enddate': today}
+# Define datasets for analysis:
+dates = date2num(TSP['date'])
 
-response = requests.post(url, data=data)
+# Define figure and axes handles:
+fig, ax = plt.subplots(figsize=(1920*10/1080.0, 10))
 
-if response.status_code != 200:
-	print("Error " + response.status_code)
-else:
-	df = pandas.read_csv(StringIO(response.text)).sort_values('date')
+# Set relevant titles for window, figure, and axes:
+fig.canvas.set_window_title('All TSP Funds')
+ax.set_title('Thrift Savings Plan Funds from ' + (todaydt-timedelta(days=dd+1)).strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
+ax.set_xlabel('Close Date')
+ax.set_ylabel('Share Value ($)')
+
+# Set limits and tick intervals on the time axis:
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_major_formatter(datefmt)
+ax.xaxis.set_minor_locator(months)
+ax.set_xlim([date2num(todaydt-timedelta(days=dd+1)), date2num(TSP['date'][len(TSP['date'])-1])])
+
+# Plot prices for all funds in list:
+for fund in ['G', 'F', 'C', 'S', 'I']:
+	ax.plot_date(dates, TSP[fund + ' Fund'], '-', label=fund + ' Fund')
+
+# Display legend as well as major and minor gridlines:
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels, loc=8, ncol=len(labels), fontsize=12)
+ax.grid(which='both')
+
+# Create a directory to store images if it does not already exist:
+imgpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
+if not os.path.isdir(imgpath): os.mkdir(imgpath)
 	
-	TSP = {}
-
-	for k, v in df.to_dict('list').items():
-		kn = k.strip()
-		if len(kn) > 0:
-			TSP[kn] = v
-
-	TSP['date'] = [datetime.strptime(date, '%Y-%m-%d') for date in TSP['date']]
-
-	dates = date2num(TSP['date'])
-
-	fig, ax = plt.subplots()
+# Save a copy of the plot in the imgpath directory:
+plt.savefig(os.path.join(imgpath, '00_AllTSPFunds.png'), bbox_inches='tight')
 	
-	fig.canvas.set_window_title('All TSP Funds')
+# Display the plot:
+plt.show(block=True)
 	
-	ax.set_title('Thrift Savings Plan Funds from ' + (todaydt-timedelta(days=dd+1)).strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
-	ax.set_xlabel('Date')
+# Close the plot:
+plt.close()
+
+for img, fund in {1: 'G', 2: 'F', 3: 'C', 4: 'S', 5: 'I'}.items():
+	# Define datasets for analysis:
+	dates = np.array(date2num(TSP['date']))
+	price = np.array(TSP[fund + ' Fund'])
+	smanl = np.array(SMA(TSP[fund + ' Fund'], nl))
+	smanh = np.array(SMA(TSP[fund + ' Fund'], nh))
+
+	# Define figure and axes handles:
+	fig, ax = plt.subplots(figsize=(1920*10/1080.0, 10))
+
+	# Set relevant titles for window, figure, and axes:
+	fig.canvas.set_window_title('TSP ' + fund + ' Fund')
+	ax.set_title('Thrift Savings Plan ' + fund + ' Fund from ' + (todaydt-timedelta(days=dd+1)).strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
+	ax.set_xlabel('Close Date')
 	ax.set_ylabel('Share Value ($)')
-	
-	ax.plot_date(dates, TSP['G Fund'], '-', label="G Fund")
-	ax.plot_date(dates, TSP['F Fund'], '-', label="F Fund")
-	ax.plot_date(dates, TSP['C Fund'], '-', label="C Fund")
-	ax.plot_date(dates, TSP['S Fund'], '-', label="S Fund")
-	ax.plot_date(dates, TSP['I Fund'], '-', label="I Fund")
 
-	#manager = plt.get_current_fig_manager()
-	#manager.resize(*manager.window.maxsize())
-	#manager.window.wm_geometry("+0+0")
-
-	ax.xaxis.set_major_locator(quarters)
+	# Set limits and tick intervals on the time axis:
+	ax.xaxis.set_major_locator(months)
 	ax.xaxis.set_major_formatter(datefmt)
 	ax.xaxis.set_minor_locator(months)
-	
 	ax.set_xlim([date2num(todaydt-timedelta(days=dd+1)), date2num(TSP['date'][len(TSP['date'])-1])])
 
-	ax.legend(loc=2)
-	
-	plt.grid(which='both')
+	# Plot price and short term and long term moving averages:
+	ax.plot_date(dates, price, '-', label="Close Values")
+	ax.plot_date(dates, smanl, '-', label=str(nl) + " Day SMA")
+	ax.plot_date(dates, smanh, '-', label=str(nh) + " Day SMA")
 
-	plt.savefig(os.path.join(imgpath, '00_AllTSPFunds.png'), bbox_inches='tight')
+	# Detect and print exact crossover signals:
+	print(fund + ' fund crossover points:')
+	crossovers = detectCrossovers(dates, smanl, smanh)
 
+	# Plot buy and sell crossover signals:
+	ax.plot(crossovers[0][0], crossovers[0][1], 'go', label="Buy Signals")
+	ax.plot(crossovers[1][0], crossovers[1][1], 'ro', label="Sell Signals")
+
+	# Display legend as well as major and minor gridlines:
+	handles, labels = ax.get_legend_handles_labels()
+	ax.legend(handles, labels, loc=8, ncol=len(labels), fontsize=12)
+	ax.grid(which='both')
+
+	# Save a copy of the plot in the imgpath directory:
+	plt.savefig(os.path.join(imgpath, '0' + str(img) + '_TSP' + fund + 'Fund.png'), bbox_inches='tight')
+
+	# Display the plot:
 	plt.show(block=True)
 
+	# Close the plot:
 	plt.close()
-
-	for img, fund in {1: 'G', 2: 'F', 3: 'C', 4: 'S', 5: 'I'}.items():
-		datesall = date2num(TSP['date'])
-		datesnl = date2num(TSP['date'][nl - 1:])
-		datesnh = date2num(TSP['date'][nh - 1:])
-
-		t = np.array(date2num(TSP['date']))
-		f = np.array(SMA(TSP[fund + ' Fund'], nl))
-		g = np.array(SMA(TSP[fund + ' Fund'], nh))
-
-		idx = np.argwhere(np.diff(np.sign(f-g))[nh:] != 0).reshape(-1) + nh
-		
-		fig, ax = plt.subplots()
-		
-		fig.canvas.set_window_title('TSP ' + fund + ' Fund')
-		
-		ax.set_title('Thrift Savings Plan ' + fund + ' Fund from ' + (todaydt-timedelta(days=dd+1)).strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
-		ax.set_xlabel('Date')
-		ax.set_ylabel('Share Value ($)')
-
-		ax.plot_date(datesall, TSP[fund + ' Fund'], '-', label=fund + " Fund")
-		ax.plot_date(datesall, SMA(TSP[fund + ' Fund'], nl), '-', label=fund + " Fund (" + str(nl) + " day)")
-		ax.plot_date(datesall, SMA(TSP[fund + ' Fund'], nh), '-', label=fund + " Fund (" + str(nh) + " day)")
-
-		crossovers = [[[],[]],[[],[]]]
-		print(fund + ' fund crossover points:')
-		for i in idx:
-			if f[i] < g[i]:
-				j = 0 # buy
-				sys.stdout.write("B: ")
-			else:
-				j = 1 # sell
-				sys.stdout.write("S: ")
-			
-			fm = (f[i+1]-f[i])/(t[i+1]-t[i])
-			gm = (g[i+1]-g[i])/(t[i+1]-t[i])
-			
-			te = (g[i]-f[i])/(fm-gm)+t[i]
-			fe = fm*(te-t[i])+f[i]
-			
-			crossovers[j][0].append(te)
-			crossovers[j][1].append(fe)
-			
-			print(num2date(te).strftime("%m/%d/%Y"))
-		
-		ax.plot(crossovers[0][0], crossovers[0][1], 'go')
-		ax.plot(crossovers[1][0], crossovers[1][1], 'ro')
-		
-		#manager = plt.get_current_fig_manager()
-		#manager.resize(*manager.window.maxsize())
-		#manager.window.wm_geometry("+0+0")
-
-		ax.xaxis.set_major_locator(quarters)
-		ax.xaxis.set_major_formatter(datefmt)
-		ax.xaxis.set_minor_locator(months)
-	
-		ax.set_xlim([date2num(todaydt-timedelta(days=dd+1)), date2num(TSP['date'][len(TSP['date'])-1])])
-		
-		ax.legend(loc=2)
-		
-		plt.grid(which='both')
-		
-		plt.savefig(os.path.join(imgpath, '0' + str(img) + '_TSP' + fund + 'Fund.png'), bbox_inches='tight')
-		
-		plt.show(block=True)
-		
-		plt.close()
