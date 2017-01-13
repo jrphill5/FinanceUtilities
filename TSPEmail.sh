94e42445ac79ad6a8a182110fa6827bd348217d3
James Phillips <jrphill5@users.noreply.github.com>
2017-01-12 22:48:26 -0500

    Updated path to point to script directory

--- TSPEmail.sh
+++ TSPEmail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 ThriftSavingsPlan.py > TSPEmail.txt
-python3 TSPEmail.py
+python3 $(dirname "$0")/ThriftSavingsPlan.py > $(dirname "$0")/TSPEmail.txt
+python3 $(dirname "$0")/TSPEmail.py
