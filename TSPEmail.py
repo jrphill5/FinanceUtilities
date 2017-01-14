c15fdcdd0987eb1d5e8fe3cae2cf65ce6991d20b
James Phillips <jrphill5@users.noreply.github.com>
2017-01-13 23:16:00 -0500

    Changed MIMEText from plain to html

--- TSPEmail.py
+++ TSPEmail.py
@@ -16,9 +16,9 @@ msg['To'] = EMAIL_TO
 
 try:
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'TSPEmail.txt'), 'r') as fh:
-        text = MIMEText('<font face="Courier New, Courier, monospace">' + fh.read() + '</font>')
+        text = MIMEH('<font face="Courier New, Courier, monospace">' + fh.read() + '</font>', 'html')
 except:
-    text = MIMEText('<font face="Courier New, Courier, monospace">Attached are the most recent TSP charts with signals.</font>')
+    text = MIMEText('<font face="Courier New, Courier, monospace">Attached are the most recent TSP charts with signals.</font>', 'html')
 
 msg.attach(text)
 
