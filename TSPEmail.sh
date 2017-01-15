433075a5e3e2c7225648c7c8350d75d92345d9a1
James Phillips <jrphill5@users.noreply.github.com>
2017-01-14 20:09:33 -0500

    Updated email script to allow for signal only emails

--- TSPEmail.sh
+++ TSPEmail.sh
@@ -2,5 +2,11 @@
 
 rm $(dirname "$0")/TSPEmail.txt
 rm $(dirname "$0")/images/*
+
 python3 $(dirname "$0")/ThriftSavingsPlan.py > $(dirname "$0")/TSPEmail.txt
-python3 $(dirname "$0")/TSPEmail.py
+
+if [ -n "$(cat TSPEmail.txt | grep '\!\!\!')" ]
+	then python3 $(dirname "$0")/TSPEmail.py --signal
+fi
+
+python3 $(dirname "$0")/TSPEmail.py --all
