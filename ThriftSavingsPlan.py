b82e028b01b8939bdaf9c57eaf02c037013970c2
James Phillips <jrphill5@users.noreply.github.com>
2017-01-15 23:28:48 -0500

    Added comparison between invested and signaled use

--- ThriftSavingsPlan.py
+++ ThriftSavingsPlan.py
@@ -38,7 +38,7 @@ def detectCrossovers(dates, smanl, smanh):
 	crossovers = []
 
 	# Detect change in sign at every point in the difference of two source lists:
-	for i in np.where(np.diff(np.sign((smanl-smanh)[nh:])))[0].reshape(-1) + nh:
+	for i in np.where(np.diff(np.sign((smanl-smanh))))[0].reshape(-1):
 		# Compute slopes for both short term and long term SMA:
 		smanlm = (smanl[i+1]-smanl[i])/(dates[i+1]-dates[i])
 		smanhm = (smanh[i+1]-smanh[i])/(dates[i+1]-dates[i])
@@ -55,6 +55,52 @@ def detectCrossovers(dates, smanl, smanh):
 
 	# Return the completed data structure
 	return crossovers
+
+# Calculate PIP following signals:
+def calcPIPFS(t, p, crossovers, verbose=False):
+	bl = None
+	sl = None
+	if verbose: print()
+
+	# Buy share on first day of period no matter what:
+	if verbose: print('share bought on ' + num2date(t[0]).strftime('%m/%d/%y') + ' for $' + '{0:.2f}'.format(p[0]))
+	bl = (t[0], p[0])
+
+	gain = -p[0];
+	for i, (s, (ts, ps)) in enumerate(crossovers):
+		# If first signal is buy, ignore, otherwise sell:
+		if i == 0:
+			if s:
+				if verbose: sys.stdout.write('buy signal ignored')
+			else:
+				if verbose: sys.stdout.write('share sold')
+				sl = (ts, ps)
+				gain += ps
+		# Otherwise, handle intermediate signals
+		else:
+			if verbose: sys.stdout.write('share ')
+			if s:
+				if verbose: sys.stdout.write('bought')
+				bl = (ts, ps)
+				gain -= ps
+			else:
+				if verbose: sys.stdout.write('sold')
+				sl = (ts, ps)
+				gain += ps
+		if verbose: print(' on ' + num2date(ts).strftime('%m/%d/%y') + ' for $' + '{0:.2f}'.format(ps))
+
+	# If signals are over and share hasn't been sold yet, sell share on end date:
+	if sl is None or bl[0] > sl[0]:
+		if verbose: print('share sold on ' + num2date(t[len(t)-1]).strftime('%m/%d/%y') + ' for $' + '{0:.2f}'.format(p[len(p)-1]))
+		gain += p[len(p)-1]
+
+	if verbose: print('{0:+.2f}'.format(gain))
+
+	return (gain, 100*gain/p[0])
+
+# Calculate PIP when fully invested:
+def calcPIPFI(t, p):
+	return ((p[len(p)-1] - p[0]), 100*(p[len(p)-1] - p[0])/p[0])
 
 # POST values to remote webserver and download CSV reply:
 def fetchTSPData(start, end):
@@ -113,18 +159,19 @@ def genPlotTitle(fig, ax, fund):
 	ax.set_title('Thrift Savings Plan ' + fund + ' from ' + (todaydt-timedelta(days=dd+1)).strftime("%m/%d/%Y") + ' to ' + TSP['date'][len(TSP['date'])-1].strftime("%m/%d/%Y"))
 
 def printLatestCrossover(fund, crossovers):
-	sys.stdout.write(fund + ' fund latest crossover: ')
+	print()
+	print(fund + ' fund latest crossover:')
 	if crossovers:
 		s, (t, p) = crossovers[-1]
-		if s: sys.stdout.write('B ')
-		else: sys.stdout.write('S ')
+		if s: sys.stdout.write('  B ')
+		else: sys.stdout.write('  S ')
 		sys.stdout.write(num2date(t).strftime('%m/%d/%Y ('))
 		sys.stdout.write(str(daysSince(num2date(t))).rjust(len(str(dd))))
 		sys.stdout.write(' days ago) @ $')
 		sys.stdout.write('{0:.2f}'.format(p))
 		return daysSince(num2date(t)) == 0
 	else:
-		sys.stdout.write('None within ' + str(dd) + ' days!')
+		sys.stdout.write('  None within ' + str(dd) + ' days!')
 		return False
 
 def printAllCrossovers(fund, crossovers):
@@ -151,14 +198,14 @@ def plotFunds(funds):
 			cut = i - 1
 			break
 
+	# Trim all data points to be in range:
+	dates = dates[cut:]
+
 	# Initialize plot:
 	fig, ax = setupPlot()
 
 	# Set relevant titles for the window and figure:
 	genPlotTitle(fig, ax, 'All Funds')
-
-	# Trim all data points to be in range:
-	dates = dates[cut:]
 
 	# Plot prices for all funds in list:
 	for fund in funds:
@@ -190,6 +237,12 @@ def plotSMASignals(t, p, img, fund):
 			cut = i - 1
 			break
 
+	# Trim all data points to be in range:
+	dates = dates[cut:]
+	price = price[cut:]
+	smanl = smanl[cut:]
+	smanh = smanh[cut:]
+
 	# Initialize plot:
 	fig, ax = setupPlot()
 
@@ -202,12 +255,13 @@ def plotSMASignals(t, p, img, fund):
 		print(' !!!')
 	else: print('');
 
-	# Trim all data points to be in range:
-	dates = dates[cut:]
-	price = price[cut:]
-	smanl = smanl[cut:]
-	smanh = smanh[cut:]
-
+	# Print comparison between staying fully invested and following signals:
+	print(fund + ' fund performance:')
+	for desc, data in [('Invested', calcPIPFI(dates, price)), ('Signaled', calcPIPFS(dates, price, crossovers)), ('Variance', np.subtract(calcPIPFS(dates, price, crossovers), calcPIPFI(dates, price)))]:
+		sys.stdout.write('  ' + desc + ' ')
+		sys.stdout.write('{0:+7.2f}'.format(data[0]).replace('-', '-$').replace('+', '+$'))
+		print('{0:+7.1f}%'.format(data[1]))
+	
 	# Plot price and short term and long term moving averages:
 	ax.plot_date(dates, price, '-', label="Close Values")
 	ax.plot_date(dates, smanl, '-', label=str(nl) + " Day SMA")
