a6e70ef526dcadc61eee871d213a8101f7ba3e4a
James Phillips <jrphill5@users.noreply.github.com>
2020-03-12 23:10:50 -0400

    Updated plots to contain information in titles

--- IEXPlotBidAsk.py
+++ IEXPlotBidAsk.py
@@ -2,16 +2,16 @@ import os, sys, glob
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-if len(sys.argv) == 1:
-    directory = "BidAsk"
-else:
-    directory = sys.argv[1]
+if len(sys.argv) == 1: directory = "BidAsk"
+else:                  directory = sys.argv[1]
 
 if not (os.path.exists(directory) and os.path.isdir(directory)):
     print("ERROR: '%s' not a valid directory!" % directory)
     exit(1)
 
 dpi = 100
+
+updated = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
 PA = {}; SA = {}
 PB = {}; SB = {}
@@ -48,10 +48,24 @@ for t in TSA: VSA.append(SA[t])
 for t in TPB: VPB.append(PB[t])
 for t in TSB: VSB.append(SB[t])
 
+if len(TPA) > 0 or len(TPB) > 0:
+    dprecent = sorted(TPA+TPB)[-1].strftime("%Y/%m/%d %H:%M:%S")
+if len(TSA) > 0 or len(TSB) > 0:
+    dsrecent = sorted(TSA+TSB)[-1].strftime("%Y/%m/%d %H:%M:%S")
+else:            askprice = None
+if len(VPA) > 0: askprice = VPA[-1]
+else:            askprice = None
+if len(VPB) > 0: bidprice = VPB[-1]
+else:            bidprice = None
+if len(VSA) > 0: asksize  = VSA[-1]
+else:            asksize  = None
+if len(VSB) > 0: bidsize  = VSB[-1]
+else:            bidsize  = None
+
 plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
 plt.step(TSA, VSA, "-", where="post", label="Asks")
 plt.step(TSB, VSB, "-", where="post", label="Bids")
-plt.title("Size vs. Time [Last Updated %s]" % datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+plt.title("Size Time Series [Ask: %d] [Bid: %d] [Spread: %d] [Recent: %s] [Updated: %s]" % (asksize, bidsize, asksize-bidsize, dprecent, updated))
 plt.xlabel("Date/Time")
 plt.ylabel("Size")
 plt.legend()
@@ -60,7 +74,7 @@ plt.savefig(os.path.join(directory, "PlotSizeTime.png"), bbox_inches="tight")
 plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
 plt.step(TPA, VPA, "-", where="post", label="Asks")
 plt.step(TPB, VPB, "-", where="post", label="Bids")
-plt.title("Price vs. Time [Last Updated %s]" % datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+plt.title("Price Time Series [Ask: $%.2f] [Bid: $%.2f] [Spread: $%.2f] [Recent: %s] [Updated: %s]" % (askprice, bidprice, askprice-bidprice, dsrecent, updated))
 plt.xlabel("Time")
 plt.ylabel("Price ($)")
 plt.legend()
