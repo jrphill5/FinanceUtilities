e820a57115241fa2e612e56bc46734810788293d
James Phillips <jrphill5@users.noreply.github.com>
2020-03-13 00:03:34 -0400

    Fixed error with plots and None values

--- IEXPlotBidAsk.py
+++ IEXPlotBidAsk.py
@@ -50,9 +50,10 @@ for t in TSB: VSB.append(SB[t])
 
 if len(TPA) > 0 or len(TPB) > 0:
     dprecent = sorted(TPA+TPB)[-1].strftime("%Y/%m/%d %H:%M:%S")
+else: dprecent = None
 if len(TSA) > 0 or len(TSB) > 0:
     dsrecent = sorted(TSA+TSB)[-1].strftime("%Y/%m/%d %H:%M:%S")
-else:            askprice = None
+else: dsrecent = None
 if len(VPA) > 0: askprice = VPA[-1]
 else:            askprice = None
 if len(VPB) > 0: bidprice = VPB[-1]
@@ -65,8 +66,20 @@ else:            bidsize  = None
 plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
 plt.step(TSA, VSA, "-", where="post", label="Asks")
 plt.step(TSB, VSB, "-", where="post", label="Bids")
-plt.title("Size Time Series [Ask: %d] [Bid: %d] [Spread: %d] [Recent: %s] [Updated: %s]" % (asksize, bidsize, asksize-bidsize, dprecent, updated))
-plt.xlabel("Date/Time")
+sizetitle = "Size Time Series [Ask: "
+if asksize is not None: sizetitle += "%d" % asksize
+else:                   sizetitle += "None"
+sizetitle += "] [Bid: "
+if bidsize is not None: sizetitle += "%d" % bidsize
+else:                   sizetitle += "None"
+sizetitle += "] [Spread: "
+if asksize is not None and bidsize is not None:
+    sizetitle += "%d" % (asksize - bidsize)
+else:
+    sizetitle += "None"
+sizetitle += "] [Recent: %s] [Updated: %s]" % (dprecent, updated)
+plt.title(sizetitle)
+plt.xlabel("Time")
 plt.ylabel("Size")
 plt.legend()
 plt.savefig(os.path.join(directory, "PlotSizeTime.png"), bbox_inches="tight")
@@ -74,7 +87,19 @@ plt.savefig(os.path.join(directory, "PlotSizeTime.png"), bbox_inches="tight")
 plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
 plt.step(TPA, VPA, "-", where="post", label="Asks")
 plt.step(TPB, VPB, "-", where="post", label="Bids")
-plt.title("Price Time Series [Ask: $%.2f] [Bid: $%.2f] [Spread: $%.2f] [Recent: %s] [Updated: %s]" % (askprice, bidprice, askprice-bidprice, dsrecent, updated))
+pricetitle = "Price Time Series [Ask: "
+if askprice is not None: pricetitle += "$%.2f" % askprice
+else:                    pricetitle += "None"
+pricetitle += "] [Bid: "
+if bidprice is not None: pricetitle += "$%.2f" % bidprice
+else:                    pricetitle += "None"
+pricetitle += "] [Spread: "
+if askprice is not None and bidprice is not None:
+    pricetitle += "$%.2f" % (askprice - bidprice)
+else:
+    pricetitle += "None"
+pricetitle += "] [Recent: %s] [Updated: %s]" % (dprecent, updated)
+plt.title(pricetitle)
 plt.xlabel("Time")
 plt.ylabel("Price ($)")
 plt.legend()
